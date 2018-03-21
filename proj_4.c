0ccb5d2e277f035e546d6283654700e9fb6c4b6a
Tabitha Holloway <tabitha.holloway@uky.edu>
2018-03-21 17:46:42 -0400

    got the tokenizing figured out

--- proj_4.c
+++ proj_4.c
@@ -3,27 +3,57 @@
 #include <stdlib.h>
 #include <string.h>
 
-
+#define SIZE 255
 #define ARGUMENTS 1
 #define TOKENSPLIT " \t\r\n\a"
 #define TOKENSIZE 255
 
 char **splitLine(char *user_answer)
 {
-	char *token;
-	token=strtok(user_answer, TOKENSPLIT);
+	char* token;
+	char **token_storage; //array that store tokens
+
+	//tokenizing *********************************
+	token = strtok(user_answer, " ");
+
+	for(int i = 0; token != NULL; i += 1)
+	{
+
+
+
+
+
+
+		//		printf("%s\n", token);
+		token_storage[i] = token;
+		token = strtok(NULL, " ");
+	}
+
+	return token_storage;
 }
+
+
+
+
+
+
+
+
+
+
+
+
 void done()
 {
 	int exit_status;
 	scanf("done %i",&exit_status);
 	printf("%i", exit_status);
-        exit(exit_status);
+	exit(exit_status);
 }
 
 void setPrompt(char * newPrompt)
 {
-	
+
 }
 
 
@@ -39,32 +69,77 @@ void setPrompt(char * newPrompt)
 
 
 int main(int argc, const char *argv[]) {
+	char setvar [] = "Setvar";
 
-char user_answer [200];
-int exit_status;
-char c;
-char prompt [200];
-        //if the argument count is not equal to ARGUMENTS, or the value of 2
-        if (argc != ARGUMENTS)
-        {
-                //prints to the user how to execute the program
-                printf("Usage: ./prog_4\n");
-        }
+	char **array;
+	char user_answer[SIZE];
+	int exit_status;
+	char c;
+	char prompt [200];
+	//if the argument count is not equal to ARGUMENTS, or the value of 2
+	if (argc != ARGUMENTS)
+	{
+		//prints to the user how to execute the program
+		printf("Usage: ./prog_4\n");
+	}
 	else
 	{
 		strcpy(prompt, "msh");
 		while(1)
 		{
 			printf("%s > ", prompt);
-			scanf("%s", user_answer);
-			if ((strcmp(user_answer, "done") == 0))
+
+			fgets(user_answer, SIZE, stdin);
+
+
+
+
+			/*			for (int k = 0; k < sizeof(user_answer); k++)
+						{
+
+						if (user_answer[k] == '\"')
+						{
+						k++;
+
+						while(user_answer[k] != '\"')
+						{
+						printf("%s", user_answer[k]);
+						k++;
+
+						}
+
+
+						}
+
+						else {
+						array = splitLine(user_answer);
+						}
+
+
+						}
+
+			 */
+
+
+
+
+			array = splitLine(user_answer);//**************************
+
+//			printf("%s\n", array[0]);
+			/*
+			   printf("%s1\n", setvar);
+			   int r = strcmp(setvar, array[0]);
+			   printf("%i", r);*/
+			/*			if ((strcmp(user_answer, "done") == 0))
+						{
+						done();
+						}*/
+			if (strcmp(user_answer,"setvar") == 0)
 			{
-				done();
+
+				printf("hey");
 			}
-			else if ((strcmp(user_answer, "setPrompt") == 0))
-			{
-			}
-			
+
 		}
 	}
 }
