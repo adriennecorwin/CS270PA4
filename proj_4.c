0dd49ac7bea1fe861e0cc84138946dce3f9f1096
Adrienne Corwin <adriennecorwin@gmail.com>
2018-03-21 08:44:16 -0400

    attempt to tokenize

--- proj_4.c
+++ proj_4.c
@@ -5,12 +5,26 @@
 
 
 #define ARGUMENTS 1
+#define TOKENSPLIT " \t\r\n\a"
+#define TOKENSIZE 255
 
+char **splitLine(char *user_answer)
+{
+	char *token;
+	token=strtok(user_answer, TOKENSPLIT);
+}
+void done()
+{
+	int exit_status;
+	scanf("done %i",&exit_status);
+	printf("%i", exit_status);
+        exit(exit_status);
+}
 
-
-
-
-
+void setPrompt(char * newPrompt)
+{
+	
+}
 
 
 
@@ -29,35 +43,28 @@ int main(int argc, const char *argv[]) {
 char user_answer [200];
 int exit_status;
 char c;
+char prompt [200];
         //if the argument count is not equal to ARGUMENTS, or the value of 2
         if (argc != ARGUMENTS)
         {
                 //prints to the user how to execute the program
                 printf("Usage: ./prog_4\n");
-
         }
-
 	else
 	{
+		strcpy(prompt, "msh");
 		while(1)
 		{
-			printf("msh > ");
-			scanf("%s %i", user_answer, &exit_status);
-
-
-if((c = getchar()) == EOF )
-{
-
-exit(0);
-
-}
-
-
-			else if (strcmp(user_answer, "done") == 0)
+			printf("%s > ", prompt);
+			scanf("%s", user_answer);
+			if ((strcmp(user_answer, "done") == 0))
 			{
-				printf("%i", exit_status);
-				exit(exit_status);
+				done();
 			}
+			else if ((strcmp(user_answer, "setPrompt") == 0))
+			{
+			}
+			
 		}
 	}
 }
