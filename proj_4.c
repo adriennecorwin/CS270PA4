a7e389a3eec4e0e56be74d4a9b1218c922fb8406
Adrienne Corwin <adriennecorwin@gmail.com>
2018-03-25 21:05:37 -0400

    tokenizing

--- proj_4.c
+++ proj_4.c
@@ -5,33 +5,63 @@
 
 #define SIZE 255
 #define ARGUMENTS 1
-#define TOKENSPLIT " \t\r\n\a"
+#define TOKENSPLIT "\"  \t\r\n\a\"\""
 #define TOKENSIZE 255
 
-char **splitLine(char *user_answer)
+char **splitLine(char user_answer[])
 {
 	char* token;
 	char **token_storage; //array that store tokens
-
+	int quote_count=0;
+//	int* positions[SIZE];
 	//tokenizing *********************************
-	token = strtok(user_answer, " ");
-
-	for(int i = 0; token != NULL; i += 1)
-	{
-		//		printf("%s\n", token);
-		token_storage[i] = token;
-		token = strtok(NULL, " ");
+//	int size = sizeof(user_answer);
+//	printf("size: %i", size);
+	for (int j=0; user_answer[j]!=NULL; j+=1){
+//		positions[j]=0;
+		if (user_answer[j] == '"'){
+			quote_count+=1;
+		//	positions[j]=1;
+		}
+		printf("count: %i\n", quote_count);
+	}
+	if (user_answer[0] == '"'){
+		printf("HELLO");
+		token = strtok(user_answer, "\"");
+		for(int i = 0; token != NULL; i += 1)
+		{
+			printf("token: %s\n", token);
+			token_storage[i] = token;
+			token = strtok(NULL, TOKENSPLIT);
+			printf("token_storage: %s\n", token_storage[i]);
+		}
+	}
+	else{
+		int j=0;
+		while (quote_count!=2){
+			if (user_answer[j] == '"'){
+				quote_count+=1;
+			}
+			j++;
+		}
+	/*	char* ans1;
+		strncpy(ans1, user_answer, j);
+		printf("%i\n", j);
+		for (int i=0; i<j; i++){
+			ans1[i]=user_answer[i];
+		}
+		printf("substring: %s\n", ans1);*/
+		token = strtok(user_answer, TOKENSPLIT);
+		for(int i = 0; token != NULL; i += 1){
+			printf("token: %s\n", token);
+			token_storage[i] = token;
+			token = strtok(NULL, "\"");
+			printf("token_storage: %s\n", token_storage[i]);
+		}
 	}
 
 	return token_storage;
 }
-
-
-
-
-
-
-
 
 
 
@@ -49,15 +79,6 @@ void setPrompt(char * newPrompt)
 {
 
 }
-
-
-
-
-
-
-
-
-
 
 
 
@@ -118,7 +139,7 @@ int main(int argc, const char *argv[]) {
 
 
 			array = splitLine(user_answer);//**************************
-
+			array = NULL;
 //			printf("%s\n", array[0]);
 			/*
 			   printf("%s1\n", setvar);
