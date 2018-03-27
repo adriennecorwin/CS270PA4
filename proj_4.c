2baf408414c30e28ee64bd565f81d662a5a87c2c
Adrienne Corwin <adriennecorwin@gmail.com>
2018-03-27 19:42:57 -0400

    tokenization complete

--- proj_4.c
+++ proj_4.c
@@ -10,57 +10,72 @@
 
 char **splitLine(char user_answer[])
 {
-	char* token;
-	char **token_storage; //array that store tokens
-	int quote_count=0;
-//	int* positions[SIZE];
-	//tokenizing *********************************
-//	int size = sizeof(user_answer);
-//	printf("size: %i", size);
-	for (int j=0; user_answer[j]!=NULL; j+=1){
-//		positions[j]=0;
-		if (user_answer[j] == '"'){
-			quote_count+=1;
-		//	positions[j]=1;
-		}
-		printf("count: %i\n", quote_count);
+	char **token_storage = (char**)malloc(SIZE*sizeof(char*)); //array that store tokens
+	for (int i=0; i<SIZE; i++){
+		token_storage[i]=(char*)malloc(SIZE*sizeof(char));
 	}
-	if (user_answer[0] == '"'){
-		printf("HELLO");
-		token = strtok(user_answer, "\"");
-		for(int i = 0; token != NULL; i += 1)
-		{
-			printf("token: %s\n", token);
-			token_storage[i] = token;
-			token = strtok(NULL, TOKENSPLIT);
-			printf("token_storage: %s\n", token_storage[i]);
-		}
-	}
-	else{
-		int j=0;
-		while (quote_count!=2){
-			if (user_answer[j] == '"'){
-				quote_count+=1;
+
+	int store_index = 0;
+	int token_index = 0;
+	char token[SIZE];
+	int open_quote=0, end_quote=0, open_space=0, end_space=0;
+	for(int i=0; i<strlen(user_answer); i++){
+		if (user_answer[i]=='"'){
+			for (int i=0; i<SIZE; i++){
+				token[i]='\0';
 			}
-			j++;
+			open_quote=i;
+			printf("openquote: %i\n", open_quote);
+			while(user_answer[i+1]!='"' && i<strlen(user_answer)){
+				i++;
+			}
+			end_quote=i+1;
+			i++;
+			printf("q: %i\n", end_quote);
+			token_index=0;
+			for(int j=open_quote+1; j<end_quote; j++){
+				token[token_index]=user_answer[j];
+				token_index++;
+			}
+			printf("s: %i\n", store_index);
+				for (int k = 0; k<strlen(token); k++){
+					token_storage[store_index][k]=token[k];
+				}
+//			token_storage[store_index]=token;
+			printf("token: %s\n", token_storage[store_index]);
+			store_index++;
 		}
-	/*	char* ans1;
-		strncpy(ans1, user_answer, j);
-		printf("%i\n", j);
-		for (int i=0; i<j; i++){
-			ans1[i]=user_answer[i];
-		}
-		printf("substring: %s\n", ans1);*/
-		token = strtok(user_answer, TOKENSPLIT);
-		for(int i = 0; token != NULL; i += 1){
-			printf("token: %s\n", token);
-			token_storage[i] = token;
-			token = strtok(NULL, "\"");
-			printf("token_storage: %s\n", token_storage[i]);
+		else if((user_answer[i]==' ' && user_answer[i-1]!='"') || (user_answer[i]=='\n' && user_answer[i-1]!='"')){
+			for (int i=0; i<SIZE; i++){
+				token[i]='\0';
+			}
+			printf("si: %i\n", store_index);
+			end_space=i;
+			while(user_answer[i-1]!=' ' && i-1>=0){
+				i--;
+			}
+			open_space=i-1;
+			printf("openspace: %i\n", open_space);
+			i=end_space;
+			token_index = 0;
+			for(int j=open_space+1; j<end_space; j++){
+				printf("j: %i\n", j);
+				token[token_index]=user_answer[j];
+				token_index++;
+			}
+			printf("s: %i\n", store_index);
+		//	token_storage[store_index]=token;
+				for (int k=0; k<strlen(token); k++){
+					token_storage[store_index][k]=token[k];
+				}
+			printf("token: %s\n", token_storage[store_index]);
+		//	printf("token: %s\n", token_storage[0]);
+			store_index++;
+			printf("endspace: %i\n", end_space);
 		}
 	}
 
-	return token_storage;
+return token_storage;
 }
 
 
@@ -75,21 +90,12 @@ void done()
 	exit(exit_status);
 }
 
-void setPrompt(char * newPrompt)
-{
-
-}
-
 
 
 
 int main(int argc, const char *argv[]) {
-	char setvar [] = "Setvar";
-
 	char **array;
 	char user_answer[SIZE];
-	int exit_status;
-	char c;
 	char prompt [200];
 	//if the argument count is not equal to ARGUMENTS, or the value of 2
 	if (argc != ARGUMENTS)
@@ -139,8 +145,8 @@ int main(int argc, const char *argv[]) {
 
 
 			array = splitLine(user_answer);//**************************
-			array = NULL;
-//			printf("%s\n", array[0]);
+			printf("%s\n", array[0]);
+//			printf("%s\n", array[1]);
 			/*
 			   printf("%s1\n", setvar);
 			   int r = strcmp(setvar, array[0]);
@@ -149,11 +155,11 @@ int main(int argc, const char *argv[]) {
 						{
 						done();
 						}*/
-			if (strcmp(user_answer,"setvar") == 0)
-			{
+						if (strcmp(array[0],"setvar") == 0)
+						{
 
-				printf("hey");
-			}
+						printf("hey");
+						}
 
 		}
 	}
